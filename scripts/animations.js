92dd842925afbf1f1e2cc2564af0763ad04ba74e
Ike <85350135+ikeawesom@users.noreply.github.com>
2023-06-06 12:52:56 +0800

    added smooth scrolling

--- scripts/animations.js
+++ scripts/animations.js
@@ -12,7 +12,7 @@ const titleanimation = () => {
     })
 }
 
-const scrollanimation = () => {
+const fadeinanimation = () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
@@ -28,9 +28,55 @@ const scrollanimation = () => {
     hiddenElements.forEach((el) => observer.observe(el));
 }
 
+function smoothScroll() {
+    function smoothScrollHelper(target, duration) {
+        var target = document.querySelector(target);
+        var targetPosition = target.getBoundingClientRect().top - 100;
+        var startPosition = window.scrollY;
+        var startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            var timeElapsed = currentTime - startTime
+            var run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run) // (x-dir, y-dir)
+
+            if (timeElapsed < duration) requestAnimationFrame(animation)
+        }
+
+        function ease(t, b, c, d) {
+            t /= d/2;
+            if (t < 1) return c/2*t*t*t*t*t + b;
+            t -= 2;
+            return c/2*(t*t*t*t*t + 2) + b;
+        };
+
+        requestAnimationFrame(animation);
+    }
+    
+    const navLinks = document.querySelectorAll("header a");
+    navLinks.forEach((link) => {
+        link.addEventListener('click',()=>{
+            var section = link.href
+            var selector = "#" + section.split("#")[1];
+            if (selector === "#") {
+                smoothScrollHelper("#landing",2000)
+            } else {
+                smoothScrollHelper(selector, 2000);
+            }
+        })
+    })
+}
+
+function vh(percent) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (percent * h) / 100;
+}
+
 const runAnimations = () => {
     titleanimation();
-    scrollanimation();
+    fadeinanimation();
+    smoothScroll();
 }
 
 runAnimations()
